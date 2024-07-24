e2d8676eff57c1148798813d43a471988d2425ce
jyotisman2 <124511911+jyotisman2@users.noreply.github.com>
2024-07-24 13:36:21 +0530

    Add files via upload
    zerodha is a stock broker, in this project i try to build replica of their website

---
+++ FINAL CODE/INDEX.js
@@ -0,0 +1,25 @@
+var a=1;
+a=2;
+console.log(a);
+const b =5;
+
+console.log(b)
+let j=1;
+
+console.log(j);
+
+let firstname="jyotisman";
+let secondname="dutta";
+let age=18;
+let ismarried= false;
+console.log("this person is "  + firstname  +  secondname + "his age is"  +  18  + " he is marrien" +  ismarried);
+
+if (ismarried== true) {
+    console.log(firstname +" is married");
+} else {
+    console.log(firstname +" is not married")  
+}
+
+
+
+
---
+++ FINAL CODE/index2.js
@@ -0,0 +1,10 @@
+
+
+let answer=0;
+for (let i = 0; i <= 1000; i++) {
+    answer = answer+i;
+    
+}
+console.log("answer is " + answer)
+
+const userinput= prompt("please enter the number");
